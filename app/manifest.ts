588923b9fce24e05d50cf5276dfc2b9a493bfabc
Maki <maki_22@hotmail.fr>
2026-05-25 19:38:23 +0200

    feat: add id field to PWA manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,6 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'Désamianteurs.com',
     short_name: 'Désamianteurs',
     description: "Trouvez un professionnel certifié amiante. Devis gratuits, pros vérifiés.",
+    id: '/',
     start_url: '/',
     display: 'standalone',
     background_color: '#0A0A0A',
